a1b3aca9fb7064000c87aca83ffddfd518fcd694
Carlos henrique C S <79725152+CarlosHCS001@users.noreply.github.com>
2025-11-22 12:35:01 -0300

    Update main.py

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,5 @@
-
 """
-Rotas principais do site (home, sobre, etc)
+Rotas principais
 """
 from flask import Blueprint, render_template
 from models import Product
@@ -10,20 +9,16 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    """Página inicial"""
-    # Pegar produtos em destaque (primeiros 6)
+    # pega os 6 primeiros produtos pra mostrar na home
     produtos_destaque = Product.query.limit(6).all()
-    
     return render_template('index.html', produtos=produtos_destaque)
 
 
 @main_bp.route('/sobre')
 def sobre():
-    """Página sobre nós"""
     return render_template('sobre.html')
 
 
 @main_bp.route('/contato')
 def contato():
-    """Página de contato"""
     return render_template('contato.html')
